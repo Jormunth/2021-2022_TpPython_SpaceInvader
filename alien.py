c494ce5247180d6ef39dfabb5a8d1b304c1bef38
Alix Deleule <alix.deleule@gmail.com>
2021-12-13 15:55:03 +0100

    debut class entity, aliens

---
+++ alien.py
@@ -0,0 +1,18 @@
+from entity import Entity
+from tkinter import Canvas
+
+class Alien(Entity):
+    def __init__(self, canvas, lives):
+        super().__init__(canvas,lives,1,0)
+
+class AlienWeak(Alien):
+    def __init__(self, canvas, lives):
+        super().__init__(canvas,lives)
+        self.__object = self.canvas.create_rectangle(0, 0, 10, 10,fill="orange")
+        self.__object.after(1000,self.move)
+
+class AlienStrong(Alien):
+    def __init__(self, canvas):
+        super().__init__(canvas,10)
+        self.__object = self.canvas.create_rectangle(0, 0, 10, 10,fill="red")
+
---
+++ entity.py
@@ -0,0 +1,13 @@
+class Entity():
+    def __init__(self,canvas,lives,dx,dy):
+        self.__lives = lives
+        self.canvas = canvas
+        self.__object = None
+        self.__dx = dx
+        self.__dy = dy
+        
+    def move(self):
+        self.canvas.move(self.__object,self.__dx,self.__dy)
+
+    def addObj():
+        pass
