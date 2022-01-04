39a7b6e916be0f135e12acdf266667586775c3a2
pisugtook <augustinlaprais@icloud.com>
2022-01-04 18:36:47 +0100

    g

--- entity.py
+++ entity.py
@@ -2,12 +2,16 @@ from tkinter.constants import X
 
 
 class Entity():
-    def __init__(self,canvas,lives,dx,dy):
+    def __init__(self,canvas,lives,dx,dy,X1,Y1,X2,Y2):
         self.__lives = lives
         self.canvas = canvas
         self.__object = None
         self.__dx = dx
         self.__dy = dy
+        self.x1 = X1
+        self.y1 = Y1
+        self.x2 = X2
+        self.y2 = Y2
         
     def setObj(self,obj):
         self.__object = obj
